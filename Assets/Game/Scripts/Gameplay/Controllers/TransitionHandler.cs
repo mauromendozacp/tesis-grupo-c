6dad016bc547b0c62323cf2b1a48e99bbe1f557a
Juan <juanrodriguezgiles@gmail.com>
2022-10-09 14:48:08 -0300

    Update transition logic/visuals.

--- Assets/Game/Scripts/Gameplay/Controllers/TransitionHandler.cs
+++ Assets/Game/Scripts/Gameplay/Controllers/TransitionHandler.cs
@@ -1,19 +1,20 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TransitionHandler : MonoBehaviour
 {
     #region EXPOSED_FIELDS
     [SerializeField] private Animation cutOffAnimation = null;
-    #endregion
+    [SerializeField] private Image backgroundImage = null;
+    #endregion 
 
     #region PRIVATE_FIELDS
     private PlayerController playerController = null;
-    private const string fadeInAnimation = "cutOffIn";
-    private const string fadeOutAnimation = "cutOffOut";
+    private const string fadeInAnimation = "fadeIn";
+    private const string fadeOutAnimation = "fadeOut";
     #endregion
 
     #region PUBLIC_METHODS
@@ -25,9 +26,11 @@ public class TransitionHandler : MonoBehaviour
     public void StartLevelTransition(Action fadeInFinished, Action fadeOutFinished)
     {
         cutOffAnimation.gameObject.SetActive(true);
+        StartCoroutine(FadeImage(true, backgroundImage));
         StartCoroutine(PlayAnimation(fadeInAnimation, animationFinished: () =>
         {
             fadeInFinished?.Invoke();
+            StartCoroutine(FadeImage(false, backgroundImage));
             StartCoroutine(PlayAnimation(fadeOutAnimation, animationFinished: () =>
             {
                 fadeOutFinished?.Invoke();
@@ -41,11 +44,33 @@ public class TransitionHandler : MonoBehaviour
     IEnumerator PlayAnimation(string clipName, Action animationFinished)
     {
         float lenght = cutOffAnimation.GetClip(clipName).length;
-        cutOffAnimation.Play(fadeInAnimation);
+        cutOffAnimation.Play(clipName);
 
         yield return new WaitForSeconds(lenght);
 
         animationFinished?.Invoke();
+    }
+    
+    IEnumerator FadeImage(bool fadeIn, Image img)
+    {
+        if (!fadeIn)
+        {
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                img.color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+          
+        }
+        else
+        {
+            for (float i = 0; i <= 3; i += Time.deltaTime)
+            {
+                img.color = new Color(0, 0, 0, i);
+                yield return null;
+            }
+          
+        }
     }
     #endregion
 }
